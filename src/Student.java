4cc78de824fe7dcddcedfb2f92011a448e179db5
Elifyildizz <yildiz.elifffff@gmail.com>
2023-07-10 15:25:42 +0300

    Student Project

---
+++ src/Student.java
@@ -0,0 +1,96 @@
+public class Student {
+    /*
+    bir öğrenci classı olucak: adı, soyadı, okul no., sınıfı, not1 not2 not3 bilgileri de olucak
+    bir öğrenci oluşturmak için en az ad ve soyad gerekmektedir
+    diğer bütün özellikler için getter setterlar yazılıcaktır
+    öğrencinin adı ve soyadı değişrilemez ama okunabilir
+    classta set not isminde bir method olucaktır,
+    bu method aynı anda bir veya iki veya üç tane not setlenebilicek şekilde yazılıcak/overload.
+     */
+    private String name;
+    private String surname;
+    private int id;
+    private static int idCounter = 1;
+    private int classN;
+    private double grade1, grade2, grade3;
+    private double gpa;
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        id = idCounter;
+        idCounter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getClassN() {
+        return classN;
+    }
+
+    public double getGrade1() {
+        return grade1;
+    }
+
+    public double getGrade2() {
+        return grade2;
+    }
+
+    public double getGrade3() {
+        return grade3;
+    }
+
+    public double calculateGPA() {
+        return (grade1 + grade2 + grade3) / 3.0;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setClassN(int classN) {
+        if (classN < 0 && classN > 4) {
+            System.out.println("Class should be between 1 to 4.");
+        } else {
+            this.classN = classN;
+        }
+    }
+
+    public void setGrade(double grade1) {
+        this.grade1 = grade1;
+        this.gpa = calculateGPA();
+    }
+
+    public void setGrade(double grade1, double grade2) {
+        this.grade1 = grade1;
+        this.grade2 = grade2;
+        this.gpa = calculateGPA();
+    }
+
+    public void setGrade(double grade1, double grade2, double grade3) {
+        this.grade1 = grade1;
+        this.grade2 = grade2;
+        this.grade3 = grade3;
+        this.gpa = calculateGPA();
+    }
+
+    public String toString() {
+        return name +
+                " " + surname +
+                "\nId:" + id +
+                "\nGrade1: " + grade1 +
+                "\nGrade2:" + grade2 +
+                "\nGrade3:" + grade3 +
+                "\nGPA: " + gpa;
+    }
+}
